6b7905630e234ae1443a926d0695e1bdb1d39823
Marta Lendinez <casandra.lendinez@outlook.com>
2026-04-12 16:42:13 +0200

    Create basic plugn setup

---
+++ ai-design-reflection-assistant/plugin/code.js
@@ -0,0 +1,2 @@
+figma.showUI(__html__, { width: 360, height: 480 });
+figma.ui.postMessage({ type: "INIT", message: "Plugin loaded ✅" });
---
+++ ai-design-reflection-assistant/plugin/code.ts
@@ -0,0 +1,3 @@
+figma.showUI(__html__, { width: 360, height: 480 });
+
+figma.ui.postMessage({ type: "INIT", message: "Plugin loaded ✅" });
